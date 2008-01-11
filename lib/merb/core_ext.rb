ebc1882679d0f4a0158bc5e22d716b7e678698fd
Yehuda Katz <wycats@gmail.com>
2008-01-11 14:52:29 -0800

    Simple changes

--- lib/merb/core_ext.rb
+++ lib/merb/core_ext.rb
@@ -1,15 +1,3 @@
-corelib = File.dirname(__FILE__) + '/core_ext'
+corelib = File.dirname(__FILE__) / "core_ext"
 
-%w[ inflector
-    class
-    kernel
-    object
-    mash
-    enumerable
-    module
-    string
-    hash
-    numeric
-    symbol
-    get_args
-  ].each {|fn| require File.join(corelib, fn)}
+Dir.glob["#{corelib}/*"].each {|fn| require (corelib / fn)}
