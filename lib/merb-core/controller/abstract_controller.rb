89961441bfd99b80ec458e616726585eba8a95cc
Yehuda Katz <wycats@yehuda-katzs-macbook-pro.local>
2008-02-05 22:35:11 -0800

    Helpers should be namespaced.

--- lib/merb-core/controller/abstract_controller.rb
+++ lib/merb-core/controller/abstract_controller.rb
@@ -125,7 +125,7 @@ class Merb::AbstractController
 
     def inherited(klass)
       _abstract_subclasses << klass.to_s
-      klass.send(:include, Object.full_const_get("#{klass}Helper")) rescue nil
+      klass.send(:include, Object.full_const_get("Merb::#{klass}Helper")) rescue nil
       super
     end
     
