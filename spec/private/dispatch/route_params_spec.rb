bb0138a3a47b2826a169197a6d957e06997d9e8a
Michael D. Ivey <ivey@gweezlebur.com>
2008-02-07 16:36:16 -0600

    Add prelim spec for route_params
    Spec passes, but issue still persists.  Hmm...

---
+++ spec/private/dispatch/route_params_spec.rb
@@ -0,0 +1,19 @@
+require File.join(File.dirname(__FILE__), "spec_helper")
+require 'rack/mock'
+require 'stringio'
+Merb.start :environment => 'test', 
+           :adapter =>  'runner', 
+           :merb_root => File.dirname(__FILE__) / 'fixture'
+
+describe Merb::Dispatcher, "route params" do
+  before(:each) do
+    env = Rack::MockRequest.env_for("/foo/bar/54")
+    env['REQUEST_URI'] = "/foo/bar/54"  # MockRequest doesn't set this
+    @controller = Merb::Dispatcher.handle(env, StringIO.new)
+  end
+
+  it "should properly set the route params" do
+    @controller.request.route_params[:id].should == '54'
+  end
+
+end
