b70bb25c670c6ecaee1c5ab068f61af1cb1b8971
Michael D. Ivey <ivey@gweezlebur.com>
2008-02-09 00:31:03 -0600

    use 3 digit version string

--- lib/merb-core/version.rb
+++ lib/merb-core/version.rb
@@ -2,7 +2,7 @@
 
 # DOC: Yehuda Katz FAILED
 module Merb
-  VERSION = '0.9' unless defined?(Merb::VERSION)
+  VERSION = '0.9.0' unless defined?(Merb::VERSION)
 
   # Merb::RELEASE meanings:
   # 'dev'   : unreleased
