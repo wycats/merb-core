d6afdf2cce03c26fca912de57e6d112aaf2dcfd2
Jonathan Younger <jonathan@daikini.com>
2008-02-12 08:40:37 -0700

    Updated merb sample applications so they use the proper configurations.
    The merb flattened samples applications were calling Merb.start
    directly which were causing issues when the applications were
    being run with the merb command. Instead, the sample applications
    now update the Merb::Config directly rather than passing them as
    options to Merb.start. The merb command calls Merb.start and does
    not need to be run from within the sample applications.
    
    Also added a default route for '/'.

--- sample/merb-very-flat/merb-very-flat.rb
+++ sample/merb-very-flat/merb-very-flat.rb
@@ -1,7 +1,5 @@
-require 'rubygems'
-require 'merb-core'
-
 Merb::Router.prepare do |r|
+  r.match('/').to(:controller => 'foo', :action =>'index')
   r.default_routes
 end
 
@@ -27,13 +25,14 @@ class Foo < Merb::Controller
   
 end
 
-Merb.start :environment        => 'production',
-           :adapter            => 'thin',
-           :framework          => {},
-           :log_level          => 'debug',
-           :use_mutex          => false,
-           :session_store      => 'cookie',
-           :session_secret_key => '5b7d26c4d99b922929b7c30ce06be0fd58a71500',
-           :exception_details  => true,
-           :reload_classes     => true,
-           :reload_time        => 0.5
+Merb::Config.use { |c|
+  c[:environment]         = 'production',
+  c[:framework]           = {},
+  c[:log_level]           = 'debug',
+  c[:use_mutex]           = false,
+  c[:session_store]       = 'cookie',
+  c[:session_secret_key]  = '5b7d26c4d99b922929b7c30ce06be0fd58a71500',
+  c[:exception_details]   = true,
+  c[:reload_classes]      = true,
+  c[:reload_time]         = 0.5
+}
