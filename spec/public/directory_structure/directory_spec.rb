f831928bb570bc1b9c804c4dd5e6987112c4a90a
Dudley Flanders <dudley@misnomer.us>
2008-02-12 11:15:26 -0600

    Allow Merb.root_path to take a list of directories
    Signed-off-by: Ezra Zygmuntowicz <ez@engineyard.com>

--- spec/public/directory_structure/directory_spec.rb
+++ spec/public/directory_structure/directory_spec.rb
@@ -4,28 +4,42 @@ Merb.start :environment => 'test',
            :merb_root => File.dirname(__FILE__) / "directory"
 
 describe "The default Merb directory structure" do
-  
+
   it "should load in controllers" do
     calling { Base }.should_not raise_error
   end
-  
+
   it "should be able to complete the dispatch cycle" do
     controller = dispatch_to(Base, :string)
     controller.body.should == "String"
   end
-  
+
   it "should be able to complete the dispatch cycle with templates" do
     controller = dispatch_to(Base, :template)
     controller.body.should == "Template ERB"
   end
-  
+
 end
 
 describe "Modifying the _template_path" do
-  
+
   it "should move the templates to a new location" do
     controller = dispatch_to(Custom, :template)
     controller.body.should == "Wonderful Template"
   end
-  
+
+end
+
+describe "Merb.root_path" do
+
+  it "should return a path relative to Merb.root" do
+    path = Merb.root_path('/app/controllers/base.rb')
+    path.should == File.join(Merb.root, '/app/controllers/base.rb')
+  end
+
+  it "should accept multiple arguments like File.join" do
+    path = Merb.root_path('app', 'controllers', 'base.rb')
+    path.should == File.join(Merb.root, 'app', 'controllers', 'base.rb')
+  end
+
 end
