wycats/merb-core

commit 47d4122d73eaf6186f1f7077f2a4a171287d7634

use REQUEST_PATH when matching routes so webrick does throw hissy fit.

lib/merb-core/dispatch/request.rb
@@ -173,7 +173,7 @@ module Merb
     
     # returns he request uri.
     def uri
-      @env['REQUEST_URI']
+      @env['REQUEST_PATH']
     end
 
     def user_agent

commit 461fed0e765813503ff7c826f2dd1741fc417f91

Modifying "use REQUEST_PATH when matching routes so webrick does throw hissy fit."
Ez: 47d4122d73eaf6186f1f7077f2a4a171287d7634 broke specs.  Does this change still
address the Webrick issue adequately?

lib/merb-core/dispatch/request.rb
@@ -173,7 +173,7 @@ module Merb
     
     # returns he request uri.
     def uri
-      @env['REQUEST_PATH']
+      @env['REQUEST_URI'] || @env['REQUEST_PATH']
     end
 
     def user_agent