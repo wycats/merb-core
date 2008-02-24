a6e69682fa3b1513a9a0afbad4f6be2a5e660ec1
Ezra Zygmuntowicz <ez@engineyard.com>
2008-02-24 15:58:55 -0800

    fix loading of environment.rb files

--- lib/merb-core/bootloader.rb
+++ lib/merb-core/bootloader.rb
@@ -83,9 +83,8 @@ class Merb::BootLoader::Dependencies < Merb::BootLoader
     end
     require initfile if File.exists?(initfile + ".rb")
 
-    if !Merb.environment.nil? && File.exist?(Merb.root_path("config", "environments") / (Merb.environment + ".rb"))
-      require Merb.root_path("config", "environments") / Merb.environment
-    end
+  if !Merb.environment.nil? && File.exist?(Merb.root_path("config", "environments") / (Merb.environment + ".rb"))
+    require Merb.root_path("config", "environments") / Merb.environment    end
   end
 end
 
