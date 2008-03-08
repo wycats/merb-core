1b663b88e39615ac0c08f0041fff8b33b0adbd20
Ezra Zygmuntowicz <ez@engineyard.com>
2008-03-08 15:54:38 -0800

    make load order of core_ext explicit

--- lib/merb-core/core_ext.rb
+++ lib/merb-core/core_ext.rb
@@ -1,3 +1,12 @@
 corelib = File.join(File.dirname(__FILE__), "core_ext")
 
-Dir.glob("#{corelib}/*.rb").each {|fn| require fn}
+require "#{corelib}/string"
+require corelib/:class
+require corelib/:hash
+require corelib/:kernel
+require corelib/:mash
+require corelib/:object
+require corelib/:object_space
+require corelib/:rubygems
+require corelib/:set
+
