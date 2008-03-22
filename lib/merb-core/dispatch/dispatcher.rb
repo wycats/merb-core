86b036c0461529a04ee003be5bab7051f633e10d
Michael D. Ivey <ivey@gweezlebur.com>
2008-03-22 17:32:32 -0500

    Make controller.route work

--- lib/merb-core/dispatch/dispatcher.rb
+++ lib/merb-core/dispatch/dispatcher.rb
@@ -64,6 +64,7 @@ class Merb::Dispatcher
 
       controller = dispatch_action(klass, action, request)
       controller._benchmarks[:dispatch_time] = Time.now - start
+      controller.route = Merb::Router.routes[route_index]
       Merb.logger.info controller._benchmarks.inspect
       Merb.logger.flush
 
