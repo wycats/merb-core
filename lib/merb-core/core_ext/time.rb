a83054ce442fe0808e7c95554c712c36af939042
Matt Todd <chiology@gmail.com>
2008-03-28 20:22:21 -0400

    Added Time#to_json to Core Extensions, making the default JSON formatted output for Time objects ISO 8601 compatible.
    Signed-off-by: Ezra Zygmuntowicz <ez@engineyard.com>

---
+++ lib/merb-core/core_ext/time.rb
@@ -0,0 +1,13 @@
+class Time
+  
+  # ==== Returns
+  # String::
+  #   ISO 8601 compatible rendering of the Time object's properties.
+  # 
+  # ==== Examples
+  #   Time.now.to_json # => "\"2008-03-28T17:54:20-05:00\""
+  def to_json
+    self.xmlschema.to_json
+  end
+  
+end
