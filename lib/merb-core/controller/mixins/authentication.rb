e5c813842b6ff61499809037eede88bca127a13b
Jonas Nicklas <jonas.nicklas@gmail.com>
2008-04-05 19:45:58 +0200

    added documentation

--- lib/merb-core/controller/mixins/authentication.rb
+++ lib/merb-core/controller/mixins/authentication.rb
@@ -1,10 +1,57 @@
 module Merb::AuthenticationMixin
   
+  # Attempts to authenticate the user via HTTP Basic authentication. Takes a
+  # block with the username and password, if the block yields false the
+  # authentication is not accepted and :halt is thrown.
+  #
+  # If no block is passed, +basic_authentication+, the +request+ and +authenticate+
+  # methods can be chained. These can be used to independently request authentication
+  # or confirm it, if more control is desired.
+  #
+  # ==== Parameters
+  # realm<~to_s>:: The realm to authenticate against. Defaults to 'Application'.
+  # &authenticator:: A block to check if the authentication is valid.
+  #
+  # ==== Examples
+  #     class Application < Merb::Controller
+  #     
+  #       before :authenticate
+  #     
+  #       protected
+  #     
+  #       def authenticate
+  #         basic_authentication("My App") do |username, password|
+  #           password == "secret"
+  #         end
+  #       end
+  #     
+  #     end
+  #
+  #     class Application < Merb::Controller
+  #     
+  #       before :authenticate
+  #     
+  #       def authenticate
+  #         user = basic_authentication.authenticate do |username, password|
+  #           User.authenticate(username, password)
+  #         end
+  #     
+  #         if user
+  #           @current_user = user
+  #         else
+  #           basic_authentication.request
+  #         end
+  #       end
+  #     
+  #     end
+  #
+  #---
+  # @public
   def basic_authentication(realm = "Application", &authenticator)
     BasicAuthentication.new(self, realm, &authenticator)
   end
   
-  class BasicAuthentication
+  class BasicAuthentication #:nodoc:
     # So we can have access to the status codes
     include Merb::ControllerExceptions
 
