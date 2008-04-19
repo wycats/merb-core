92df5521823b8d4bb272845c522ef722dd206149
Michael S. Klishin <michael@novemberain.com>
2008-04-20 00:24:52 +0300

    Clean up empty lines in Kernel extensions.

--- spec/private/core_ext/kernel_spec.rb
+++ spec/private/core_ext/kernel_spec.rb
@@ -1,11 +1,9 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-
 describe "Kernel#require" do
-
   before do
     @logger = StringIO.new
-    Merb.logger = Merb::Logger.new(@logger)    
+    Merb.logger = Merb::Logger.new(@logger)
   end
 
   it "should be able to require and throw a useful error message" do
@@ -13,22 +11,22 @@ describe "Kernel#require" do
     Merb.logger.should_receive(:error!).with("foo")
     Kernel.rescue_require("redcloth", "foo")
   end
-
-  
 end
 
+
+
 describe "Kernel#caller" do
-  
   it "should be able to determine caller info" do
     __caller_info__.should be_kind_of(Array)
   end
-  
+
   it "should be able to get caller lines" do
     __caller_lines__(__caller_info__[0], __caller_info__[1], 4).length.should == 9
     __caller_lines__(__caller_info__[0], __caller_info__[1], 4).should be_kind_of(Array)
   end
-  
 end
+
+
 
 describe "Kernel misc." do
   it "should extract options from args" do
@@ -36,9 +34,9 @@ describe "Kernel misc." do
     Kernel.extract_options_from_args!(args).should == {:baz => :bar}
     args.should == ["foo", "bar"]
   end
-  
+
   it "should throw a useful error if there's no debugger" do
-    Merb.logger.should_receive(:info!).with "\n***** Debugger requested, but was not " + 
+    Merb.logger.should_receive(:info!).with "\n***** Debugger requested, but was not " +
                         "available: Start server with --debugger " +
                         "to enable *****\n"
     Kernel.debugger
