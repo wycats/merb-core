124f9dc07c6ce29df74e0d688f9d8794e644f819
Michael S. Klishin <michael@novemberain.com>
2008-04-19 20:21:47 +0300

    Report total spec suite run time.

--- lib/merb-core/test/run_specs.rb
+++ lib/merb-core/test/run_specs.rb
@@ -1,4 +1,5 @@
 require 'open3'
+require 'benchmark'
 
 # Runs specs in all files matching the file pattern.
 #
@@ -6,28 +7,32 @@ require 'open3'
 # globs<String, Array[String]>:: File patterns to look for.
 # spec_cmd<~to_s>:: The spec command. Defaults to "spec".
 # run_opts<String>:: Options to pass to spec commands, for instance,
-#                  if you want to use profiling formatter.
+#                    if you want to use profiling formatter.
 def run_specs(globs, spec_cmd='spec', run_opts = "-c -f s")
   require "optparse"
   require "spec"
   globs = globs.is_a?(Array) ? globs : [globs]
   examples, failures, errors, pending = 0, 0, 0, 0
-  globs.each do |glob|
-    Dir[glob].each do |spec|
-      response = Open3.popen3("#{spec_cmd} #{File.expand_path(spec)} #{run_opts}") do |i,o,e|
-        while out = o.gets
-          STDOUT.puts out
-          STDOUT.flush
-          if out =~ /\d+ example/
-            e, f, p = out.match(/(\d+) examples?, (\d+) failures?(?:, (\d+) pending?)?/)[1..-1]
-            examples += e.to_i; failures += f.to_i; pending += p.to_i
+
+  time = Benchmark.measure do
+    globs.each do |glob|
+      Dir[glob].each do |spec|
+        response = Open3.popen3("#{spec_cmd} #{File.expand_path(spec)} #{run_opts}") do |i,o,e|
+          while out = o.gets
+            STDOUT.puts out
+            STDOUT.flush
+            if out =~ /\d+ example/
+              e, f, p = out.match(/(\d+) examples?, (\d+) failures?(?:, (\d+) pending?)?/)[1..-1]
+              examples += e.to_i; failures += f.to_i; pending += p.to_i
+            end
           end
+          errors += 1 if e.is_a?(IO)
+          STDOUT.puts e.read if e.is_a?(IO)
         end
-        errors += 1 if e.is_a?(IO)
-        STDOUT.puts e.read if e.is_a?(IO)
       end
     end
   end
+
   puts
   puts "*** TOTALS ***"
   if failures == 0
@@ -36,5 +41,7 @@ def run_specs(globs, spec_cmd='spec', run_opts = "-c -f s")
     print "\e[31m"
   end
   puts "#{examples} examples, #{failures} failures, #{errors} errors, #{pending} pending"
+
+  puts "suite run in #{time.real} seconds"
   print "\e[0m"
 end
