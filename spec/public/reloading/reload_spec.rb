0c2827eb402897f1b2e12d236988ae72044c4503
Michael S. Klishin <michael@novemberain.com>
2008-04-20 05:18:21 +0300

    Use a constant for sleep time in reloading specs

--- spec/public/reloading/reload_spec.rb
+++ spec/public/reloading/reload_spec.rb
@@ -1,18 +1,19 @@
 require File.join(File.dirname(__FILE__), "..", "..", "spec_helper")
 Merb.start :environment => 'test',
            :merb_root => File.dirname(__FILE__) / "directory"
-           
+
 describe "The reloader" do
+  SLEEP_TIME = 0.5
 
   def reload!
     Merb::BootLoader::ReloadClasses.reload
   end
-  
+
   before :all do
     @reload_file = File.dirname(__FILE__) / "directory" / "app" / "controllers" / "reload.rb"
     File.open(@reload_file, "w") do |f|
       @text = <<-END
-        
+
         class Reloader < Application
         end
 
@@ -21,20 +22,20 @@ describe "The reloader" do
       END
       f.puts @text
     end
-    
-    sleep 0.6
+
+    sleep SLEEP_TIME
   end
-  
+
   it "should reload files that were changed" do
     defined?(Hello).should_not be_nil
     defined?(Reloader).should_not be_nil
     defined?(Reloader2).should be_nil
-    
-    sleep 0.6
-    
+
+    sleep SLEEP_TIME
+
     File.open(@reload_file, "w") do |f|
       f.puts <<-END
-        
+
         class Reloader < Application
         end
 
@@ -43,17 +44,17 @@ describe "The reloader" do
       END
     end
 
-    sleep 0.6
-    
+    sleep SLEEP_TIME
+
     defined?(Hello).should be_nil
     defined?(Reloader).should_not be_nil
     defined?(Reloader2).should_not be_nil
   end
-  
+
   it "should remove classes for _abstract_subclasses" do
     File.open(@reload_file, "w") do |f|
       f.puts <<-END
-        
+
         class Reloader < Application
         end
 
@@ -61,18 +62,18 @@ describe "The reloader" do
         end
       END
     end
-    
-    sleep 0.6
-    
+
+    sleep SLEEP_TIME
+
     Merb::AbstractController._abstract_subclasses.should include("Reloader")
-    Merb::AbstractController._abstract_subclasses.should include("Reloader2")    
+    Merb::AbstractController._abstract_subclasses.should include("Reloader2")
     defined?(Hello).should be_nil
     defined?(Reloader).should_not be_nil
-    defined?(Reloader2).should_not be_nil    
+    defined?(Reloader2).should_not be_nil
   end
-  
+
   after :each do
-    sleep 0.6
+    sleep SLEEP_TIME
     File.open(@reload_file, "w") do |f|
       f.puts @text
     end
