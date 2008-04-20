97ffa0f2b8f63776527f759e47192e64148fb28b
Michael S. Klishin <michael@novemberain.com>
2008-04-20 21:17:36 +0300

    Clean up String extensions spec.

--- spec/private/core_ext/string_spec.rb
+++ spec/private/core_ext/string_spec.rb
@@ -160,8 +160,6 @@ describe String, "#relative_path_from" do
     2.times { two_levels_up.pop } # remove two deepest directories
     two_levels_up = File::SEPARATOR + two_levels_up.join(File::SEPARATOR)
 
-    puts two_levels_up
-    puts site_dir
     two_levels_up.relative_path_from(site_dir).should == "../.."
   end
 end
