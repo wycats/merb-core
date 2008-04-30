caef0bf262a53072b6700a84b87a9e40f3a75e90
Michael S. Klishin <michael@novemberain.com>
2008-04-30 08:05:45 +0300

    Example (from merb_sequel) and corrections to the way Merb finds out plugins Rakefiles.

--- lib/merb-core/plugins.rb
+++ lib/merb-core/plugins.rb
@@ -1,5 +1,5 @@
 module Merb
-  
+
   module Plugins
 
     # ==== Returns
@@ -9,15 +9,27 @@ module Merb
     def self.config
       @config ||= File.exists?(Merb.root / "config" / "plugins.yml") ? YAML.load(File.read(Merb.root / "config" / "plugins.yml")) || {} : {}
     end
-    
+
     # ==== Returns
-    # Array:: All Rakefiles for plugins.
+    # Array(String):: All Rakefile load paths Merb uses for plugins.
     def self.rakefiles
       Merb.rakefiles
     end
 
     # ==== Parameters
     # *rakefiles:: Rakefiles to add to the list of plugin Rakefiles.
+    #
+    # ==== Notes
+    #
+    # This is a recommended way to register your plugin's Raketasks
+    # in Merb.
+    #
+    # ==== Examples
+    # From merb_sequel plugin:
+    #
+    # if defined(Merb::Plugins)
+    #   Merb::Plugins.add_rakefiles "merb_sequel" / "merbtasks"
+    # end
     def self.add_rakefiles(*rakefiles)
       Merb.add_rakefiles *rakefiles
     end
