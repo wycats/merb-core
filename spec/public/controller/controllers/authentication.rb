b8c2dd2f976afa946aa78dc6c797d6b0e70f3f20
Michael S. Klishin <michael@novemberain.com>
2008-05-14 19:14:55 +0300

    More sweet empty lines management in controller fixtures.

--- spec/public/controller/controllers/authentication.rb
+++ spec/public/controller/controllers/authentication.rb
@@ -1,47 +1,45 @@
 module Merb::Test::Fixtures::Controllers
-  
+
   class Testing < Merb::Controller
     self._template_root = File.dirname(__FILE__) / "views"
   end
 
   class BasicAuthentication < Testing
-
     before :authenticate, :only => :index
 
     def index
       "authenticated"
     end
-    
+
     protected
-  
+
     def authenticate
       basic_authentication { |u, p| u == "Fred" && p == "secret" }
     end
-    
   end
-  
+
   class BasicAuthenticationWithRealm < BasicAuthentication
     def authenticate
       basic_authentication("My Super App") { |u, p| u == "Fred" && p == "secret" }
     end
   end
-  
+
   class AuthenticateBasicAuthentication < Testing
     def index
       basic_authentication.authenticate { |u, p| "Fred:secret" }
     end
   end
-  
+
   class RequestBasicAuthentication < BasicAuthentication
     def authenticate
       basic_authentication.request
     end
   end
-  
+
   class RequestBasicAuthenticationWithRealm < BasicAuthentication
     def authenticate
       basic_authentication("My SuperApp").request
     end
   end
-  
+
 end
--- spec/public/controller/controllers/responder.rb
+++ spec/public/controller/controllers/responder.rb
@@ -1,23 +1,23 @@
-
-
 module Merb::Test::Fixtures::Controllers
-
   class Testing < Merb::Controller
     self._template_root = File.dirname(__FILE__) / "views"
   end
 
+
   class Responder < Testing
-    
     def index
       render
     end
   end
 
+
   class HtmlDefault < Responder; end
+
 
   class ClassProvides < Responder
     provides :xml
   end
+
 
   class LocalProvides < Responder
     def index
@@ -26,37 +26,41 @@ module Merb::Test::Fixtures::Controllers
     end
   end
 
+
   class MultiProvides < Responder
     def index
       provides :html, :js
       render
     end
   end
-  
+
+
   class ClassOnlyProvides < Responder
     only_provides :text, :xml
-    
+
     def index
       "nothing"
     end
   end
-  
+
+
   class OnlyProvides < Responder
     def index
       only_provides :text, :xml
       "nothing"
     end
   end
-    
+
   class ClassDoesntProvides < Responder
     provides :xml
     does_not_provide :html
-    
+
     def index
       "nothing"
     end
   end
-  
+
+
   class DoesntProvide < Responder
     def index
       provides :xml
@@ -64,20 +68,19 @@ module Merb::Test::Fixtures::Controllers
       "nothing"
     end
   end
-  
+
+
   class FooFormatProvides < Responder
     only_provides :foo
-    
+
     def index
       render "nothing"
     end
-    
+
     def show
       headers["Content-Language"] = 'nl'
       headers["Biz"] = "buzz"
       render "nothing"
     end
-    
   end
-  
 end
