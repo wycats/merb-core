fabef07c9668ef32565ccecff97f969280242e1d
Yehuda Katz <wycats@gmail.com>
2008-05-17 19:05:05 -0700

    Convert string.rb over to YARD format.

--- lib/merb-core/core_ext/string.rb
+++ lib/merb-core/core_ext/string.rb
@@ -1,31 +1,33 @@
 require "pathname"
 
 class String
-
-  # ==== Returns
-  # String:: The string with all regexp special characters escaped.
+  
+  ##
+  # @return <String> The string with all regexp special characters escaped.
   #
-  # ==== Examples
+  # @example
   #   "*?{}.".escape_regexp #=> "\\*\\?\\{\\}\\."
   def escape_regexp
     Regexp.escape self
   end
-
-  # ==== Returns
-  # String:: The string with all regexp special characters unescaped.
+  
+  ##
+  # @return String The string with all regexp special characters unescaped.
   #
-  # ==== Examples
+  # @example
   #   "\\*\\?\\{\\}\\.".unescape_regexp #=> "*?{}."
   def unescape_regexp
     self.gsub(/\\([\.\?\|\(\)\[\]\{\}\^\$\*\+\-])/, '\1')
   end
-
-  # ==== Returns
-  # String:: The string converted to snake case.
+  
+  ##
+  # @return <String> The string converted to snake case.
   #
-  # ==== Examples
+  # @example
   #   "FooBar".snake_case #=> "foo_bar"
+  # @example
   #   "HeadlineCNNNews".snake_case #=> "headline_cnn_news"
+  # @example
   #   "CNN".snake_case #=> "cnn"
   def snake_case
     return self.downcase if self =~ /^[A-Z]+$/
@@ -33,57 +35,56 @@ class String
       return $+.downcase
   end
 
-  # ==== Returns
-  # String:: The string converted to camel case.
+  ##
+  # @return <String> The string converted to camel case.
   #
-  # ==== Examples
+  # @example
   #   "foo_bar".camel_case #=> "FooBar"
   def camel_case
     return self if self !~ /_/ && self =~ /[A-Z]+.*/
     split('_').map{|e| e.capitalize}.join
   end
 
-  # ==== Returns
-  # String:: The path string converted to a constant name.
+  ##
+  # @return <String> The path string converted to a constant name.
   #
-  # ==== Examples
+  # @example
   #   "merb/core_ext/string".to_const_string #=> "Merb::CoreExt::String"
   def to_const_string
     gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
   end
 
-  # ==== Returns
-  # String::
+  ##
+  # @return <String>
   #   The path that is associated with the constantized string, assuming a
   #   conventional structure.
   #
-  # ==== Examples
+  # @xample
   #   "FooBar::Baz".to_const_path # => "foo_bar/baz"
   def to_const_path
     snake_case.gsub(/::/, "/")
   end
 
-  # ==== Parameters
-  # o<String>:: The path component to join with the string.
+  ##
+  # @param o<String> The path component to join with the string.
   #
-  # ==== Returns
-  # String:: The original path concatenated with o.
+  # @return <String> The original path concatenated with o.
   #
-  # ==== Examples
+  # @example
   #   "merb"/"core_ext" #=> "merb/core_ext"
   def /(o)
     File.join(self, o.to_s)
   end
 
-  # ==== Parameters ====
-  # other<String>:: Base path to calculate against
+  ##
+  # @param other<String> Base path to calculate against
   #
-  # ==== Returns ====
-  # String:: Relative path from between the two
+  # @return <String> Relative path from between the two
   #
-  # ==== Example ====
-  # "/opt/local/lib".relative_path_from("/opt/local/lib/ruby/site_ruby") # => "../.."
+  # @example
+  #   "/opt/local/lib".relative_path_from("/opt/local/lib/ruby/site_ruby") # => "../.."
   def relative_path_from(other)
     Pathname.new(self).relative_path_from(Pathname.new(other)).to_s
   end
+  
 end
