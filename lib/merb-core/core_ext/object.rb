184da19953282f448d15a1163a04b68f0cfcab29
Fabien Franzen <info@atelierfabien.be>
2008-05-24 16:53:40 +0200

    Added Object.full_const_set

--- lib/merb-core/core_ext/object.rb
+++ lib/merb-core/core_ext/object.rb
@@ -86,9 +86,23 @@ class Object
   # @return <Object> The constant corresponding to the name.
   def full_const_get(name)
     list = name.split("::")
+    list.shift if list.first.blank?
     obj = Object
     list.each {|x| obj = obj.const_get(x) }
     obj
+  end
+  
+  # @param name<String> The name of the constant to get, e.g. "Merb::Router".
+  # @param value<Object> The value to assign to the constant.
+  #
+  # @return <Object> The constant corresponding to the name.
+  def full_const_set(name, value)    
+    list = name.split("::")
+    toplevel = list.first.blank?
+    list.shift if toplevel
+    last = list.pop
+    obj = list.empty? ? Object : Object.full_const_get(list.join("::"))
+    obj.const_set(last, value) if obj && !obj.const_defined?(last)
   end
 
   # Defines module from a string name (e.g. Foo::Bar::Baz)
