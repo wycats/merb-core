894c7be2c25c29b6a260f6473a80c420fe55b161
Yehuda Katz <wycats@gmail.com>
2008-05-29 17:41:58 -0700

    Make obj.full_const_get more robust

--- lib/merb-core/core_ext/object.rb
+++ lib/merb-core/core_ext/object.rb
@@ -87,8 +87,12 @@ class Object
   def full_const_get(name)
     list = name.split("::")
     list.shift if list.first.blank?
-    obj = Object
-    list.each {|x| obj = obj.const_get(x) }
+    obj = self
+    list.each do |x| 
+      # This is required because const_get tries to look for constants in the 
+      # ancestor chain, but we only want constants that are HERE
+      obj = obj.const_defined?(x) ? obj.const_get(x) : obj.const_missing(x)
+    end
     obj
   end
   
