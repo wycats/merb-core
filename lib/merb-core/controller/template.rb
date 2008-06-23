eeac765396194f17053de3ccb48db980e280a72d
Yehuda Katz <wycats@gmail.com>
2008-06-23 15:36:33 -0700

    Try #concat to resolve some issues.

--- lib/merb-core/controller/template.rb
+++ lib/merb-core/controller/template.rb
@@ -157,7 +157,11 @@ module Merb::Template
     # mod<Module>:: The module that the compiled method will be placed into.
     def self.compile_template(io, name, mod)
       template = ::Erubis::BlockAwareEruby.new(io.read)
+
+      _old_verbose, $VERBOSE = $VERBOSE, nil
       template.def_method(mod, name, File.expand_path(io.path))
+      $VERBOSE = _old_verbose
+      
       name
     end
 
@@ -199,38 +203,28 @@ end
 module Erubis
   module BlockAwareEnhancer
     def add_preamble(src)
-      src << "_old_buf, @_erb_buf = @_erb_buf, ''; "
+      src << "$VERBOSE = nil; _old_buf, @_erb_buf = @_erb_buf, ''; "
       src << "@_engine = 'erb'; "
     end
 
     def add_postamble(src)
       src << "\n" unless src[-1] == ?\n      
-      src << "_ret = @_erb_buf; @_erb_buf = _old_buf; _ret.to_s\n"
+      src << "_ret = @_erb_buf; @_erb_buf = _old_buf; _ret.to_s;\n"
     end
 
     def add_text(src, text)
-      src << " @_erb_buf << '" << escape_text(text) << "'; "
+      src << " @_erb_buf.concat '" << escape_text(text) << "'; "
     end
 
     def add_expr_escaped(src, code)
-      src << ' @_erb_buf << ' << escaped_expr(code) << ';'
-    end
-
-    def add_stmt(src, code)
-      if code =~ /(end|\})\s*\Z/
-        src << code << ');'
-        return
-      end
-      
-      src << (code =~ /(do|\{)(\s*\|[^|]*\|)?\s*\Z/ ? '( ' : '') << code
-      src << ';' unless code[-1] == ?\n        
+      src << ' @_erb_buf.concat ' << escaped_expr(code) << ';'
     end
 
     def add_expr_literal(src, code)
       if code =~ /(do|\{)(\s*\|[^|]*\|)?\s*\Z/
-        src << ' @_erb_buf << (' << code << "; "
+        src << ' @_erb_buf.concat ' << code << "; "
       else
-        src << ' @_erb_buf << (' << code << ').to_s;'
+        src << ' @_erb_buf.concat (' << code << ').to_s;'
       end
     end
   end
