e0b3cf4abfe775a20113ad0721c924fc0fd92247
Ezra Zygmuntowicz <ez@engineyard.com>
2008-06-23 23:08:30 -0700

    use Thread.pass to avoid running newly queued item before the action's thread finishes

--- lib/merb-core/dispatch/worker.rb
+++ lib/merb-core/dispatch/worker.rb
@@ -9,6 +9,12 @@ module Merb
     
     def process_queue
       while blk = Merb::Dispatcher.work_queue.pop
+         # we've been blocking on the queue waiting for an item sleeping.
+         # when someone pushes an item it wakes up this thread so we 
+         # immediately pass execution to the scheduler so we don't 
+         # accidentally run this block before the action finishes 
+         # it's own processing
+        Thread.pass
         blk.call
       end  
     end
