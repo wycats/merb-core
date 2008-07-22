0a3d655be79657b99549a5522a13847119c95f4c
Michael S. Klishin <michael@novemberain.com>
2008-07-22 03:33:21 +0300

    Move to merb-extlib is almost there.

--- lib/merb-core/core_ext.rb
+++ lib/merb-core/core_ext.rb
@@ -2,6 +2,7 @@ begin
   require "merb-extlib"
 rescue LoadError => e
   puts "Merb-core 0.9.4 and later uses merb-extlib for Ruby core class extensions. Install it from github.com/wycats/merb-extlib."
+  exit
 end
 
 corelib = File.join(File.dirname(__FILE__), "core_ext")
