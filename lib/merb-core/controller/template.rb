a869557e886ed1aa35e9bd54882a7e03684629a9
Yehuda Katz <wycats@gmail.com>
2008-07-23 15:37:00 -0700

    Fixes open IO issue

--- lib/merb-core/controller/template.rb
+++ lib/merb-core/controller/template.rb
@@ -104,8 +104,10 @@ module Merb::Template
     # @public
     def inline_template(io, mod = Merb::InlineTemplates)
       path = File.expand_path(io.path)
-      METHOD_LIST[path.gsub(/\.[^\.]*$/, "")] = 
+      ret = METHOD_LIST[path.gsub(/\.[^\.]*$/, "")] = 
         engine_for(path).compile_template(io, template_name(path), mod)
+      io.close
+      ret
     end
     
     # Finds the engine for a particular path.
