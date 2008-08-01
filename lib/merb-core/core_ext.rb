00835301ba91aa3f2e7eb20b4f71d5a94584157d
Michael S. Klishin <michael@novemberain.com>
2008-08-01 03:46:50 +0300

    merb-extlib => exlib

--- lib/merb-core/core_ext.rb
+++ lib/merb-core/core_ext.rb
@@ -1,7 +1,7 @@
 begin
-  require "merb-extlib"
+  require "extlib"
 rescue LoadError => e
-  puts "Merb-core 0.9.4 and later uses merb-extlib for Ruby core class extensions. Install it from github.com/wycats/merb-extlib."
+  puts "Merb-core 0.9.4 and later uses extlib for Ruby core class extensions. Install it from github.com/sam/extlib."
   exit
 end
 
