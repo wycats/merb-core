b0ad4d55b90ee26e5c8e4ec3218505592173eba6
Michael S. Klishin <michael@novemberain.com>
2008-08-07 06:35:07 +0300

    Make spec for #342 do correct assertions.

--- spec/public/abstract_controller/filter_spec.rb
+++ spec/public/abstract_controller/filter_spec.rb
@@ -101,5 +101,7 @@ describe Merb::AbstractController, " should support before and after filters" do
   
   it "should not get contaminated by cousins" do
     dispatch_should_make_body("FilterChild2", "Before Index")
+    dispatch_should_make_body("FilterChild1", "Before Limited", :limited)
+    dispatch_should_make_body("FilterChild1", " Index")
   end
 end
