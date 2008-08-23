aa182c307e26b0fd42d032840af0929862a78c0f
Michael S. Klishin <michael@novemberain.com>
2008-08-23 19:27:19 +0300

    Document Merb Rack application.

--- lib/merb-core/rack/application.rb
+++ lib/merb-core/rack/application.rb
@@ -1,7 +1,15 @@
 module Merb  
   module Rack
+    # Merb Rack application used by web server adapters.
     class Application
-      
+      # Called by web servser adapters. Must return 3 tuple of
+      # status, Hash of headers and body that responds to :each and
+      # yields strings (String, Proc or custom class instance).
+      #
+      # @param env<Hash>
+      #   Rack environment
+      #
+      # @return Array(~to_s, Hash, ~each)
       def call(env) 
         begin
           controller = ::Merb::Dispatcher.handle(Merb::Request.new(env))
@@ -18,6 +26,21 @@ module Merb
         controller.rack_response
       end
 
+      # Returns true for requests that should be deferred. Deferred requests
+      # supposed to be served in threads to prevent event loop of event-driven
+      # servers from blocking on long requests.
+      #
+      # Request is treated deferred if value of PATH_INFO matches regexp
+      # returned by Merb.deferred_actions. See Merb.deferred_actions documentation.
+      #
+      # @note
+      #   Thin and Ebb both support deferred requests.
+      #
+      # @param env<Hash>
+      #   Rack environment.
+      #
+      # @return
+      #   true if request is deferred
       def deferred?(env)
         path = env[Merb::Const::PATH_INFO] ? env[Merb::Const::PATH_INFO].chomp('/') : ""
         if path =~ Merb.deferred_actions
