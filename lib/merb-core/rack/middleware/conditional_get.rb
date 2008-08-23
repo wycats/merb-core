82a6c545f9cd516f2fe7abe35cc2f16bbefac630
Michael S. Klishin <michael@novemberain.com>
2008-08-23 18:25:32 +0300

    Port Django's conditional GET middleware to Rack.

---
+++ lib/merb-core/rack/middleware/conditional_get.rb
@@ -0,0 +1,23 @@
+module Merb
+  module Rack
+
+    class ConditionalGet < Merb::Rack::Middleware
+      def call(env)
+        status, headers, body = @app.call(env)
+
+        # set Date header using RFC1123 date format as specified by HTTP
+        # RFC2616 section 3.3.1.
+        if etag = headers['ETag']
+          status = 304 if etag == env[Merb::Const::HTTP_IF_NONE_MATCH]
+        end
+
+        if last_modified = headers[Merb::Const::LAST_MODIFIED]
+          status = 304 if last_modified == env[Merb::Const::HTTP_IF_MODIFIED_SINCE]
+        end
+        
+        [status, headers, body]
+      end
+    end
+    
+  end
+end
--- spec/public/rack/rack_middleware_spec.rb
+++ spec/public/rack/rack_middleware_spec.rb
@@ -1,4 +1,5 @@
 require File.join(File.dirname(__FILE__), "..", "..", "spec_helper")
+require File.join(File.dirname(__FILE__), "shared_example_groups")
 
 class RackyController < Merb::Controller
   def index
@@ -11,28 +12,6 @@ end
 
 Merb::Router.prepare do |r|
   r.match("/heavy/lifting").to(:controller => "racky_controller")
-end
-
-describe "rack application", :shared => true do
-  it 'is callable' do
-    @app.should respond_to(:call)
-  end
-
-  it 'returns a 3-tuple' do
-    @result.size.should == 3
-  end
-
-  it 'returns status as first tuple element' do
-    @result.first.should == 200
-  end
-
-  it 'returns hash of headers as the second tuple element' do
-    @result[1].should be_an_instance_of(Hash)
-  end
-
-  it 'returns response body as third tuple element' do
-    @result.last.should == @body
-  end
 end
 
 
@@ -56,20 +35,6 @@ describe Merb::Rack::Application do
 
     it "returns false when request path DOES NOT match deferred actions regexp" do
       @app.deferred?(Rack::MockRequest.env_for('/not/deferred')).should be(false)
-    end
-  end
-end
-
-
-describe "transparent middleware", :shared => true do
-  it "delegates request handling to wrapped Rack application" do
-    @result.last.should == @body
-  end
-
-  describe "#deferred?" do
-    it "is delegated to wrapped Rack application" do
-      @middleware.deferred?(@env).should be(true)
-      @middleware.deferred?(Rack::MockRequest.env_for('/not-deferred/')).should be(false)
     end
   end
 end
