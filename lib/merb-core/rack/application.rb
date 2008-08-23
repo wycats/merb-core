a2d6768045bd836a37a8a3d9bccc42cf17a9828c
Michael S. Klishin <michael@novemberain.com>
2008-08-23 18:50:38 +0300

    Make Rack application set Date header as required by RFC2616.

--- lib/merb-core/rack/application.rb
+++ lib/merb-core/rack/application.rb
@@ -6,15 +6,20 @@ module Merb
         begin
           controller = ::Merb::Dispatcher.handle(Merb::Request.new(env))
         rescue Object => e
-          return [500, {"Content-Type"=>"text/html"}, e.message + "<br/>" + e.backtrace.join("<br/>")]
+          return [500, {Merb::Const::CONTENT_TYPE => "text/html"}, e.message + "<br/>" + e.backtrace.join("<br/>")]
         end
         Merb.logger.info "\n\n"
         Merb.logger.flush
+
+        unless controller.headers[Merb::Const::DATE]
+          require "time"
+          controller.headers[Merb::Const::DATE] = Time.now.rfc2822.to_s
+        end
         controller.rack_response
       end
 
       def deferred?(env)
-        path = env['PATH_INFO'] ? env['PATH_INFO'].chomp('/') : ""
+        path = env[Merb::Const::PATH_INFO] ? env[Merb::Const::PATH_INFO].chomp('/') : ""
         if path =~ Merb.deferred_actions
           Merb.logger.info! "Deferring Request: #{path}"
           true
