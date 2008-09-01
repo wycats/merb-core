7cf3c28e0bf88b5afc346bf032e47cc3c453c666
Bryan Ray <bryansray@gmail.com>
2008-09-01 01:02:31 -0500

    Ticket #461 - This simply adds output for what host and port the adapter has started on.
    Signed-off-by: Michael S. Klishin <michael@novemberain.com>

--- lib/merb-core/server.rb
+++ lib/merb-core/server.rb
@@ -50,6 +50,7 @@ module Merb
           puts "Running bootloaders..." if Merb::Config[:verbose]
           BootLoader.run
           puts "Starting Rack adapter..." if Merb::Config[:verbose]
+          Merb.logger.info! "Starting Merb server listening at #{Merb::Config[:host]}:#{port}"
           Merb.adapter.start(Merb::Config.to_hash)
         end
       end
