cc2aaf0d96414eaf68324dbff947a7cb181de69b
Michael S. Klishin <michael@novemberain.com>
2008-09-07 01:21:01 +0300

    Finish implementation of conditonal get mixin.

--- lib/merb-core/controller/mixins/conditional_get.rb
+++ lib/merb-core/controller/mixins/conditional_get.rb
@@ -1,29 +1,30 @@
 module Merb::ConditionalGetMixin
 
   def etag=(tag)
-    headers[Merb::Const::ETAG] = %("#{expand_cache_key(tag)}")
+    headers[Merb::Const::ETAG] = %("#{tag}")
   end
 
   def etag
     headers[Merb::Const::ETAG]
   end
 
-  def etag_matches?(tag)
-    tag == headers[Merb::Const::ETAG]
+  def etag_matches?(tag = self.etag)
+    tag == self.request.if_none_match
   end
 
   def last_modified=(time)
     headers[Merb::Const::LAST_MODIFIED] = time.httpdate
   end
-  
+
   def last_modified
     Time.rfc2822(headers[Merb::Const::LAST_MODIFIED])
   end
 
-  protected
-
-  def expand_cache_key(tag)
-    tag
+  def not_modified?(time = self.last_modified)
+    request.if_modified_since && time && time <= request.if_modified_since
   end
-  
+
+  def request_fresh?
+    etag_matches?(self.etag) || not_modified?(self.last_modified)
+  end
 end
--- spec/public/controller/controllers/conditional_get.rb
+++ spec/public/controller/controllers/conditional_get.rb
@@ -7,14 +7,28 @@ module Merb::Test::Fixtures
     end
 
     class ConditionalGet < Testing
-      def etag
+      def sets_etag
         self.etag = "39791e6fb09"
         "can has etag"
       end
 
-      def last_modified
+      def sets_last_modified
         self.last_modified = Time.at(7000)
         "can has last-modified"
+      end
+
+      def superfresh
+        self.etag          = "39791e6fb09"
+        self.last_modified = Time.at(7000)
+        
+        "can has fresh request"
+      end
+
+      def stale
+        self.etag          = "1234567678"
+        self.last_modified = Time.at(9000)
+        
+        "can has stale request"
       end
     end
   end
