acb90a6e57e075717a235f5896df545937880416
Yehuda Katz <wycats@gmail.com>
2008-09-25 22:32:13 -0700

    Forking setup works without any forking.

--- lib/merb-core/rack/adapter/abstract.rb
+++ lib/merb-core/rack/adapter/abstract.rb
@@ -2,72 +2,86 @@ module Merb
   module Rack
     class AbstractAdapter
     
+      def self.spawn_worker(port)
+        pid = Kernel.fork
+        start_at_port(port, @opts) unless pid
+
+        # pid means we're in the parent, which means continue the loop
+        throw(:new_worker) unless pid
+        
+        @pids[port] = pid
+        $CHILDREN = @pids.values
+      end
+    
       def self.start(opts={})
         @opts = opts
         $CHILDREN ||= []
+        parent = nil
         
-        pids = {}
+        @pids = {}
         port = (opts[:socket] || opts[:port]).to_i
         max_port = Merb::Config[:cluster] ? Merb::Config[:cluster] - 1 : 0
-        pid = nil
         
         Merb.logger.warn! "Cluster: #{max_port}"
         
-        $0 = "merb: spawner"
+        if max_port == 0
+          start_at_port(port)
+          return
+        end
         
-        0.upto(max_port) do |i|          
-          pid = Kernel.fork
-          start_at_port(port + i, opts) unless pid
+        $0 = "merb: spawner"
 
-          # pid means we're in the parent, which means continue the loop
-          break unless pid
-          
-          $CHILDREN << pid
-          pids[port + i] = pid
+        catch(:new_worker) do
+          0.upto(max_port) do |i|
+            parent = spawn_worker(port + i)
+          end
         end
 
         # pid means we're in the parent, so start watching the children
         # no pid means we're in a child, so just move on
-        return unless pid
+        return unless parent
 
         0.upto(max_port) do |i|
           Thread.new do
-            loop do
-              pid = pids[port + i]
-              begin
-                _, status = Process.wait2(pid)
-              rescue SystemCallError
-              ensure
-                Thread.exit if !status || status.exitstatus != 0 || Merb.exiting
-              end
+            catch(:new_worker) do
+              loop do
+                pid = @pids[port + i]
+                begin
+                  _, status = Process.wait2(pid)
+                rescue SystemCallError => e
+                ensure
+                  Thread.exit if !status || status.exitstatus != 0 || Merb.exiting
+                end
               
-              new_pid = Kernel.fork
-              start_at_port(port + i, opts) unless new_pid
-              break unless new_pid
-              pids[port + i] = new_pid
-              $CHILDREN = pids.values
+                spawn_worker(port + i)
+              end
             end
           end
         end
 
-        Process.waitall
+        sleep
         
       end
       
-      def self.start_at_port(port, opts)
+      def self.start_at_port(port, opts = @opts)
         at_exit do
           Merb::Server.remove_pid(port)
         end
         
         if Merb::Config[:daemonize]
           trap('INT') do
-            puts "INT"
             stop
             Merb.logger.warn! "Exiting port #{port}\n"
             exit_process
           end
-        else
+        elsif Merb::Config[:fork_for_class_load]
           trap('INT') { 1 }
+        end
+        
+        trap('HUP') do
+          stop
+          Merb.logger.warn! "Exiting port #{port} on #{Process.pid}\n"
+          exit_process
         end
         
         trap('ABRT') do
