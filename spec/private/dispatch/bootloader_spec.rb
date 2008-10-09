b808cb9afb2165e6ecb0d7ccf0833421593179f7
Drew Colthorp <colthorp@atomicobject.com>
2008-10-09 12:47:10 -0700

    docs for the bootloader. bootloader_spec doesn't use RackUpApplication#run return value

--- spec/private/dispatch/bootloader_spec.rb
+++ spec/private/dispatch/bootloader_spec.rb
@@ -5,14 +5,16 @@ describe Merb::BootLoader::RackUpApplication do
   it "should default to rack config (rack.rb)" do
     options = {:merb_root => File.dirname(__FILE__) / 'fixture'}
     Merb::Config.setup(options)
-    app = Merb::BootLoader::RackUpApplication.run
+    Merb::BootLoader::RackUpApplication.run
+    app = Merb::Config[:app]
     app.class.should == Merb::Rack::Static
   end
 
   it "should use rackup config that we specified" do
     options = {:rackup => File.dirname(__FILE__) / 'fixture' / 'config' / 'black_hole.rb'}
     Merb::Config.setup(options)
-    app = Merb::BootLoader::RackUpApplication.run
+    Merb::BootLoader::RackUpApplication.run
+    app = Merb::Config[:app]
     app.class.should == Rack::Adapter::BlackHole
 
     env = Rack::MockRequest.env_for("/black_hole")
